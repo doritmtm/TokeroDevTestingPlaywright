3bb8316dfcc333061f3c775bbc960752368bd6eb
doritmtm <dorian_tm@yahoo.com>
2025-05-03 16:14:01 +0300

    Page title test

---
+++ PoliciesListTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Playwright;
+
+namespace TokeroDevTestingPlaywright
+{
+    [TestClass]
+    public class PoliciesListTests : PlaywrightTest
+    {
+        [TestMethod]
+        public async Task Page_Title()
+        {
+            IBrowser chrome = await Playwright.Chromium.LaunchAsync();
+            IBrowserContext chromeContext = await chrome.NewContextAsync();
+            IPage chromePagePolicies = await chromeContext.NewPageAsync();
+            await chromePagePolicies.GotoAsync($"{StaticSettings.MainWebsiteUrl}/en/policies");
+            await Expect(chromePagePolicies).ToHaveTitleAsync($"TOKERO policies and rules | {StaticSettings.Company}");
+        }
+    }
+}
---
+++ StaticSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace TokeroDevTestingPlaywright
+{
+    public static class StaticSettings
+    {
+        public static string MainWebsiteUrl { get; } = "https://tokero.dev";
+        public static string Company { get; } = "TOKERO";
+    }
+}
